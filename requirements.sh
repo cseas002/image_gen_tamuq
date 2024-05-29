b2d085a7ceace1825250dfe6ee4c70def689d9a3
cseas002 <cseas002@ucy.ac.cy>
2024-05-29 11:19:39 +0300

    First commit

---
+++ requirements.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install python3-pip
+# Run on 3.11
+#pip install jax
+#pip install jaxlib
+#pip install jax-metal
+pip install keras
+pip install numpy
+pip install matplotlib
+pip install notebook
+pip install pandas
+#pip install keras-cv
+pip install tensorflow keras_cv --upgrade --quiet
